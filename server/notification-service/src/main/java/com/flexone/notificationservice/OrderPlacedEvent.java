6752e5c9349e18cda4b014a94bb6f817e745f6be
Eric <edhavey@gmail.com>
2023-05-12 15:54:02 -0500

    Setup Kafka notification-service. Setting up Docker

---
+++ server/notification-service/src/main/java/com/flexone/notificationservice/OrderPlacedEvent.java
@@ -0,0 +1,14 @@
+package com.flexone.notificationservice;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderPlacedEvent {
+
+    private String orderId;
+
+}
